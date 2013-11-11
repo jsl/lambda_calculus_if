f7f8969106e712284a561f7dcbf93ba2905a0343
Justin Leitgeb <justin@stackbuilders.com>
2013-11-10 21:52:40 -0500

    Rename true and false following suggestion in Types and Programming Languages

--- if.rb
+++ if.rb
@@ -1,32 +1,32 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 
-l_true = -> (x) { -> (y) { x } }
-l_false = -> (x) { -> (y) { y } }
+tru = -> (x) { -> (y) { x } }
+fls = -> (x) { -> (y) { y } }
 
 if_then_else = -> (p) { -> (a) { -> (b) { p.call(a).call(b) } } }
 
 describe 'Implementing conditionals in the lambda calculus' do
-  describe 'the "true" lambda' do
+  describe 'the tru (true) lambda expression' do
     it 'returns the argument given to the first function' do
-      l_true.call('first').call('second').must_equal 'first'
+      tru.call('first').call('second').must_equal 'first'
     end
   end
 
-  describe 'the "false" lambda' do
+  describe 'the fls (false) lambda expression' do
     it 'returns the argument given to the second function' do
-      l_false.call('first').call('second').must_equal 'second'
+      fls.call('first').call('second').must_equal 'second'
     end
   end
 
   describe 'if..then..else using the lambda calculus' do
     it 'returns the value of the "then" branch if the conditional is true' do
-      if_then_else.call(l_true).call('true value').call('false value').
+      if_then_else.call(tru).call('true value').call('false value').
         must_equal 'true value'
     end
 
     it 'returns the value of the "else" branch if the conditional is false' do
-      if_then_else.call(l_false).call('true value').call('false value').
+      if_then_else.call(fls).call('true value').call('false value').
         must_equal 'false value'
     end
   end
